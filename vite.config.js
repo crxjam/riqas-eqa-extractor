34416dcb16221637e87d99ddacdad8b86652beec
James Croxford <crxjam002@uct.ac.za>
2025-12-30 11:40:20 +0200

    Fix vite.config duplicate exports

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,4 @@ export default defineConfig({
   plugins: [react()],
   base: '/riqas-eqa-extractor/',
 })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
