d93f97586eff62746a6c49fc71223c3b9c4374ab
James Croxford <crxjam002@uct.ac.za>
2026-01-06 09:20:32 +0200

    Fix CORS for GitHub Pages and expose download filename

--- api_server.py
+++ api_server.py
@@ -26,10 +26,15 @@ def root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
-    allow_credentials=True,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://crxjam.github.io",
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Content-Disposition"],
 )
 
 
